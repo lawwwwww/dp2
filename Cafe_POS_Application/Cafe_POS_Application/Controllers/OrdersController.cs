4f4004f5792f4f93ac07472e28832af30e5bb6d7
0xvader <alexhaide55@hotmail.com>
2019-10-23 21:18:18 +0800

    Added Mary's OrdersController

---
+++ Cafe_POS_Application/Cafe_POS_Application/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cafe_POS_Application.Models;
+
+namespace Cafe_POS_Application.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult order()
+        {
+            return View();
+        }
+    }
+}
